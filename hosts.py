b4f62028d99029d9910b498bafb12e0cd9dc166f
Tyler Trout <tyler@werkbot.com>
2020-01-09 13:20:26 -0500

    Working hosts updater

--- hosts.py
+++ hosts.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 
 import sys
+import re
+
+def insertAfter(filetext, regex, newText):
+  """ Inserts 'newText' into 'filetext' right after 'regex'. """
+  lastEntry = re.findall(regex, filetext)[-1]
+  index = filetext.find(lastEntry)
+  return filetext[:index + len(lastEntry)] + newText + filetext[index + len(lastEntry):]
 
 # Update this file path if necessary
 hosts = open('C:/Windows/System32/drivers/etc/hosts', 'r+')
@@ -8,19 +15,19 @@ hosts = open('C:/Windows/System32/drivers/etc/hosts', 'r+')
 projectType = sys.argv[1];
 projectName = sys.argv[2];
 
+hostsText = hosts.read()
+
 # hosts.write('127.0.0.1\t{0}.localhost'.format(projectName))
 if projectType == 'l' or projectType == 'laravel':
-	print('it is a laravel project')
+	print('Laravel project added to hosts file')
+	hostsText = insertAfter(hostsText, r'192.168.10.10\s+[\w-]+.localhost', '\n192.168.10.10\t{0}.localhost'.format(projectName))
 elif projectType == 's' or projectType == 'silverstripe':
-	print('it is a silverstripe project')
+	print('Silverstripe project added to hosts file')
+	hostsText = insertAfter(hostsText, r'127.0.0.1\s+[\w-]+.localhost', '\n127.0.0.1\t\t{0}.localhost'.format(projectName))
 else:
 	print('Invalid project type, try "l" for laravel or "s" for silverstripe')
+	sys.exit()
 
-print(hosts.read().rfind('127.0.0.1'))
-
-print('Generated hosts line for', projectName);
-
-# def insertAfter(haystack, needle, newText):
-#   """ Inserts 'newText' into 'haystack' right after 'needle'. """
-#   i = haystack.find(needle)
-#   return haystack[:i + len(needle)] + newText + haystack[i + len(needle):]
+hosts.seek(0)
+hosts.truncate()
+hosts.write(hostsText)
