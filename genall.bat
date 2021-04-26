6ad395d3909714183fbc7f0a8fd5f8844884d8d3
Tyler Trout <tiller1010@gmail.com>
2021-04-26 19:24:38 -0400

    Genall script updates

--- genall.bat
+++ genall.bat
@@ -1,3 +1,11 @@
-hosts.py $1 $2
-vhost.py $2
-env.py $1 $2
+if "%3" == "x" goto xampp
+if "%3" == "xampp" goto xampp
+hosts.py %2 h
+homestead.py %1 %2
+env.py %1 %2 h
+goto end
+:xampp
+hosts.py %2 x
+vhost.py %2
+env.py %1 %2 x
+:end
--- genall.sh
+++ genall.sh
@@ -4,8 +4,9 @@ if [[ $3 == 'x' || $3 == 'xampp' ]]
 then
 	hosts.py $2 x
 	vhost.py $2
+	env.py $1 $2 x
 else
 	hosts.py $2 h
 	homestead.py $1 $2
+	env.py $1 $2 h
 fi
-env.py $1 $2
